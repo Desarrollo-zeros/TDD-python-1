95eef05cd6bddb6fde7e966e685e9bf8e7dc74e4
Carlos Andrés Castilla García <carlos.castilla@itglobers.com>
2023-04-21 21:17:44 -0500

    Instalando FastAPI y creando TestCase

--- test.py
+++ test.py
@@ -3,3 +3,14 @@ from fastapi.testclient import TestClient
 from main import app
 import requests
 client = TestClient(app)
+
+class TestMain(unittest.TestCase):
+    def test_is_prime(self):
+        response = client.get("/IsPrime/7")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), True)
+
+    def test_fibonacci(self):
+        response = client.get("/fibonacci/8")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), 21)
