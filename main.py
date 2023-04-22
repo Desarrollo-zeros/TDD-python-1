5c88a140602b410f692c8e1e72b002217245a1e3
Carlos Andrés Castilla García <carlos.castilla@itglobers.com>
2023-04-21 21:19:16 -0500

    creando test IsPrime

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/IsPrime/{number}")
+async def is_prime(number: int):
+    if number < 2:
+        return False
+    for i in range(2, int(number / 2) + 1):
+        if (number % i) == 0:
+            return False
+    return True
