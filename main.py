9dcb84ad7048341f8b207fd2662f45a7a2e527de
Carlos Andrés Castilla García <carlos.castilla@itglobers.com>
2023-04-21 21:19:33 -0500

    creando test fibonacci

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/IsPrime/{number}")
 async def is_prime(number: int):
     if number < 2:
@@ -10,3 +11,16 @@ async def is_prime(number: int):
         if (number % i) == 0:
             return False
     return True
+
+
+@app.get("/fibonacci/{position}")
+async def fibonacci(position: int):
+    if position < 3:
+        return 1
+    a = 1
+    b = 1
+    for i in range(3, position + 1):
+        c = a + b
+        a = b
+        b = c
+    return b
